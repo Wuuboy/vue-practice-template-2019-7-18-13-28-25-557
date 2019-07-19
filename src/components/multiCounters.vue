97f9088354c56fc3411163a3f4adc65d5d4894f8
曹桂凤 <demi.cao@oocl.com>
2019-07-19 14:46:43 +0800

    [Demi]feat:add multiCounters.vue

---
+++ src/components/multiCounters.vue
@@ -0,0 +1,17 @@
+<template>
+    <div id="app">     
+        <input v-model="countersNumber" placeholder="input the number of counters"> 
+        <p>The counters number is :{{countersNumber}}</p>
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
